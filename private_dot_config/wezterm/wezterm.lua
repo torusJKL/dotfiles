e50a02091124c67721c0d12c6fed27e7247b9a3e
Gal Buki <jkl@torus.ch>
2026-03-20 11:12:26 +0200

    add WezTerm configuration

---
+++ private_dot_config/wezterm/wezterm.lua
@@ -0,0 +1,35 @@
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+
+config.colors = {
+    background = "#1e1e2e",
+}
+
+config.keys = {
+    {
+        key = ",",
+        mods = "CTRL",
+        action = wezterm.action.SendString("\x1b[1;5l"),  -- ESC [ 1 ; 5 l
+    },
+    {
+        key = "UpArrow",
+        mods = "CTRL|SHIFT",
+        action = wezterm.action.SendString("\x1b[1;6A"),  -- ESC [ 1 ; 6 A
+    },
+    {
+        key = "DownArrow",
+        mods = "CTRL|SHIFT",
+        action = wezterm.action.SendString("\x1b[1;6B"),  -- ESC [ 1 ; 6 B
+    },
+}
+
+config.font = wezterm.font_with_fallback {
+    'Cousine Nerd Font Mono',
+    'JetBrains Mono',
+}
+
+config.bidi_enabled = true
+config.hide_tab_bar_if_only_one_tab = true
+
+return config
